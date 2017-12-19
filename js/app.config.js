0393d79c4de4c3201d1978c4d2a1b2d7670a20ff
Juan Andres <jasrd1990@gmail.com>
2017-12-19 12:58:06 +0000

    Version de presentacion

--- js/app.config.js
+++ js/app.config.js
@@ -7,7 +7,7 @@
     function config($routeProvider, $sceProvider){
        // if (,mode == 'prod')
        // Esto evita que se muestren cosas en consola para no quitar todos los console.log, debug only
-        //console.log = () => {};
+        console.log = () => {};
        // $sceProvider.enabled(false);
         $routeProvider.when("/" , {
             controller : 'HomeController',
