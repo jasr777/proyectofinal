a79874f8c924e27279ffa6e9b2605d6b6f183ae8
Juan Andrés <jasrd1990@gmail.com>
2017-12-25 23:26:24 +0000

    arreglado rotten

--- js/controllers/HomeController.controller.js
+++ js/controllers/HomeController.controller.js
@@ -611,8 +611,7 @@
                         }
 
                         if(auxRatings[i].Source == "Rotten Tomatoes"){
-                            parsedRatings[1] = auxRatings[i].value;
-                         
+                            parsedRatings[1] = auxRatings[i].Value;
 
                         }
 
